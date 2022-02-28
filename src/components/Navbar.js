0b8dea4967bdf90a0c568f1d00b8ba638c5a7ddf
liwen-w <wong.liwen01@gmail.com>
2022-03-01 01:34:50 +0800

    second commit

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -63,12 +63,12 @@ function Navbar() {
                     </Link>
                   </li>
                   <li className='nav-item'>
-                    <Link to='/sign-up' className='nav-links-mobile' onClick={closeMobileMenu}>
-                      Sign Up
+                    <Link to='/login' className='nav-links-mobile' onClick={closeMobileMenu}>
+                      Login/Register
                     </Link>
                   </li>
                 </ul>
-                {button && <Button buttonStyle='btn--outline'>SIGN UP</Button>}
+                {button && <Button buttonStyle='btn--outline'>Login/Register</Button>}
 
 
             </div>
